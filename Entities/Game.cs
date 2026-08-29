fcc1551c11957509469877d65367957fdd296ecd
Alvin Gwak <alvingwak@gmail.com>
2026-08-28 20:09:10 -0700

    Add input validation with data annotations to Game and Movie

--- Entities/Game.cs
+++ Entities/Game.cs
@@ -1,12 +1,19 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace GameCatalog.Entities;
 
 public class Game
 {
     public int Id { get; set; }
 
+    [Required]
+    [StringLength(200)]
     public string Title { get; set; }
     
+    [Range(0, 200)]
     public decimal Price { get; set; }
 
+    [Required]
+    [StringLength(100)]
     public string Publisher { get; set; }
 }
--- Entities/Movie.cs
+++ Entities/Movie.cs
@@ -1,12 +1,19 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace GameCatalog.Entities;
 
 public class Movie
 {
     public int Id { get; set; }
 
+    [Required]
+    [StringLength(200)]
     public string Title { get; set; }
 
+    [Required]
+    [StringLength(100)]
     public string Genre { get; set; }
 
+    [Range(1888, 2100)]
     public int ReleaseYear { get; set; }
 }
